46c7b943d2fef6d7939ed4cbb65b401b8b5d6524
MrNivorous <karn.sood@gmail.com>
2019-10-25 10:48:38 -0500

    Updates

---
+++ precache-manifest.73362a2cbec4aa442b4441041a7f495d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c348163481eefc2ccdec9ea19d760436",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b91bc3b7c1ed176ac9b2",
+    "url": "/robofriends/static/css/main.ae9b9c6e.chunk.css"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/js/2.3cd86ebb.chunk.js"
+  },
+  {
+    "revision": "b91bc3b7c1ed176ac9b2",
+    "url": "/robofriends/static/js/main.ebc48bc5.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
